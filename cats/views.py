1585c47137d64af84677c4f9173c93b1863bdbb3
hatas <hataskrayu20@gmail.com>
2023-08-08 17:46:53 +0300

    s

--- cats/views.py
+++ cats/views.py
@@ -1,13 +1,20 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
+from rest_framework.throttling import ScopedRateThrottle
 
+from .throttling import WorkingHoursRateThrottle
 from .models import Achievement, Cat, User
-
+from .permissions import OwnerOrReadOnly, ReadOnly
 from .serializers import AchievementSerializer, CatSerializer, UserSerializer
 
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
+    
+    permission_classes = (OwnerOrReadOnly, ReadOnly) 
+    throttle_classes = (WorkingHoursRateThrottle, ScopedRateThrottle)
+    # А далее применится лимит low_request
+    throttle_scope = 'low_request' 
     
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -21,3 +28,8 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
 class AchievementViewSet(viewsets.ModelViewSet):
     queryset = Achievement.objects.all()
     serializer_class = AchievementSerializer
+
+def get_permissions(self):
+    if self.action == 'retrieve':
+        return (ReadOnly(),)
+    return super().get_permissions()
--- kittygram2/settings.py
+++ kittygram2/settings.py
@@ -131,12 +131,23 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated', 
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly', 
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.UserRateThrottle',
+        'rest_framework.throttling.ScopedRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'user': '10000/day', 
+        'anon': '1000/day',  
+        # Имена (ключи) для scope придумывает разработчик, 
+        # в меру собственной фантазии
+        'low_request': '1/minute',
+    }
 }
 
 SIMPLE_JWT = {
